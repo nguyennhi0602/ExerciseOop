414097c8ba7082cd6e78587ba9b623b63c3dca44
nhi <nguyenthinhi0602@gmail.com>
2019-06-12 15:40:22 +0700

    oop 1 edit 3

--- src/test/java/Exercise1Test.java
+++ src/test/java/Exercise1Test.java
@@ -25,8 +25,12 @@ public class Exercise1Test {
     public void test_type() {
         Assert.assertEquals(triangle.getType(), "tam giac can");
         Assert.assertEquals(new Exercise1(0,0,0,0,0,0).getType(), "tam giac khong hop le");
+        Assert.assertEquals(new Exercise1(0,0,3,0,-1,0).getType(), "tam giac khong hop le");
         Assert.assertEquals(new Exercise1(-3, 0, 0, 3 * Math.sqrt(3), 3, 0).getType(), "tam giac deu");
-        Assert.assertEquals(new Exercise1(0,3,0,0,2,0).getType(), "tam giac vuong");
+        Assert.assertEquals(new Exercise1(-3, 0,  3, 0,0, 3 * Math.sqrt(3)).getType(), "tam giac deu");
+        Assert.assertEquals(new Exercise1(0, 0, 0, 3, 4, 0).getType(), "tam giac vuong");
+        Assert.assertEquals(new Exercise1(4, 0, 0, 0, 0, 3).getType(), "tam giac vuong");
+        Assert.assertEquals(new Exercise1(0, 3, 4, 0, 0, 0).getType(), "tam giac vuong");
         Assert.assertEquals(new Exercise1(0,0,0,2,1,4).getType(), "tam giac binh thuong");
     }
 
