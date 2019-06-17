901a63ce5b4ac94a9d0f1d3fecb82697fb788d79
nhi <nguyenthinhi0602@gmail.com>
2019-06-17 22:12:38 +0700

    oop 4

---
+++ src/main/java/Exercise4.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercise4<T> {
+    private List<T> stackNumber = new ArrayList<>();
+
+    public List<T> push(T x) {
+        stackNumber.add(x);
+        return stackNumber;
+    }
+
+    public T pop() {
+        if (stackNumber.isEmpty()) {
+            return null;
+        }
+        T result = stackNumber.get(stackNumber.size() - 1);
+        stackNumber.remove(stackNumber.get(stackNumber.size() - 1));
+        return result;
+    }
+
+    public boolean isEmpty() {
+        return stackNumber.size() == 0;
+    }
+}
