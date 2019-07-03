4cc103e2bff963902035a50dec516db6a5ec9757
nhi <nguyenthinhi0602@gmail.com>
2019-07-03 09:29:34 +0700

    oop 3 edit 3

--- src/main/java/exercise3/StudentController.java
+++ src/main/java/exercise3/StudentController.java
@@ -7,13 +7,13 @@ import java.util.List;
 
 public class StudentController {
 
-    private static List<Student> students=new ArrayList<>();
-    private static File file =new File("D:\\test.json");
+    private static List<Student> students = new ArrayList<>();
+    private static File file = new File("D:\\test.json");
 
-    public StudentController(String filePath) {
+    public StudentController(String filePath) throws IOException {
         file = new File(filePath);
-        loadFromFile();
     }
+
     public static List<Student> getStudents() {
         return students;
     }
@@ -22,58 +22,52 @@ public class StudentController {
         return file;
     }
 
-    public static void addStudent(Student student){
+    public static void addStudent(Student student) throws IOException {
         students.add(student);
         saveToFile();
     }
 
-    public static void deleteStudent(int id) {
-        for(Student stu:students){
-            if(stu.getId()==id){
+    public static void deleteStudent(int id) throws IOException {
+        for (Student stu : students) {
+            if (stu.getId() == id) {
                 students.remove(stu);
             }
         }
         saveToFile();
     }
 
-    public static void saveToFile(){
-        try {
-            WriteReadFile.writeToJsonFile(file, students);
-        } catch (IOException e) {
-        }
+    public static void saveToFile() throws IOException {
+        WriteReadFile.writeToJsonFile(file, students);
     }
 
-    public static List<Student> loadFromFile(){
-        try {
-            students = WriteReadFile.readJsonFile(file, Student[].class);
-        } catch (IOException e) {
-        }
+    public static List<Student> loadFromFile() throws IOException {
+        students = WriteReadFile.readJsonFile(file, Student[].class);
         return students;
     }
 
-    public static Student getById(int id){
-        for(Student stu:students){
-            if(stu.getId()==id){
+    public static Student getById(int id) {
+        for (Student stu : students) {
+            if (stu.getId() == id) {
                 return stu;
             }
         }
         return null;
     }
 
-    public static List<Student> findByName(String firstName){
-        List<Student> result=new ArrayList<>();
-        for(Student stu:students){
-            if(stu.getFirstName().equals(firstName)){
+    public static List<Student> findByName(String firstName) {
+        List<Student> result = new ArrayList<>();
+        for (Student stu : students) {
+            if (stu.getFirstName().equals(firstName)) {
                 result.add(stu);
             }
         }
         return result;
     }
 
-    public static List<Student> findByClass(String className){
-        List<Student> result=new ArrayList<>();
-        for(Student stu:students){
-            if(stu.getClassName().equals(className)){
+    public static List<Student> findByClass(String className) {
+        List<Student> result = new ArrayList<>();
+        for (Student stu : students) {
+            if (stu.getClassName().equals(className)) {
                 result.add(stu);
             }
         }
--- src/test/java/exercise3test/StudentControllerTest.java
+++ src/test/java/exercise3test/StudentControllerTest.java
@@ -5,6 +5,7 @@ import exercise3.StudentController;
 import exercise3.WriteReadFile;
 import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
@@ -13,29 +14,66 @@ import java.util.List;
 
 public class StudentControllerTest {
     private static List<Student> students;
-    private static File file_origin = new File("students.json");
+    private static File file_origin = new File("student.json");
     private static File file_temp = new File("temp.json");
+//
+//    @BeforeClass
+//    public static void setup() {
+//        file_temp = new File("student.json");
+//        file_origin = new File("temp.json");
+//
+//        if (!file_origin.exists()) {
+//            try {
+//                file_origin.createNewFile();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        List<Student> students;
+//
+//        try {
+//            students = WriteReadFile.readJsonFile(file_temp, Student[].class);
+//            WriteReadFile.writeToJsonFile(file_origin, students);
+//        } catch (IOException ignored) {
+//        }
+//
+//    }
+
 
     @Test
-    public void test_addStudent() {
+    public void test_addStudent() throws IOException {
         StudentController sc = new StudentController("temp.json");
 
         int default_length = sc.getStudents().size();
 
-        sc.addStudent(Student.StudentBuilder.newStudentBuilder().setId(1).setFirstName("haha").setLastName("Le").setClassName("112").build());
-        sc.addStudent(Student.StudentBuilder.newStudentBuilder().setId(2).setFirstName("Nhi").setLastName("Nguyen").setClassName("112").build());
+        sc.addStudent(Student
+                .StudentBuilder
+                .newStudentBuilder()
+                .setId(1).setFirstName("haha")
+                .setLastName("Le")
+                .setClassName("112")
+                .build());
+        sc.addStudent(Student
+                .StudentBuilder
+                .newStudentBuilder()
+                .setId(2)
+                .setFirstName("Nhi")
+                .setLastName("Nguyen")
+                .setClassName("112")
+                .build());
         System.out.println(default_length);
         Assert.assertEquals(sc.getStudents().size(), default_length + 2);
     }
 
     @Test
-    public void test_getById() {
+    public void test_getById() throws IOException {
         StudentController sc = new StudentController("temp.json");
         Assert.assertNull(sc.getById(7));
     }
 
     @Test
-    public void test_deleteStudent() {
+    public void test_deleteStudent() throws IOException {
         StudentController sc = new StudentController("temp.json");
         int length = sc.getStudents().size();
         sc.deleteStudent(1);
@@ -43,30 +81,27 @@ public class StudentControllerTest {
     }
 
     @Test
-    public void test_loadFromFile() {
+    public void test_loadFromFile() throws IOException {
         StudentController sc = new StudentController("temp.json");
         Assert.assertNotNull(sc.loadFromFile());
     }
 
     @Test
-    public void test_findByName() {
+    public void test_findByName() throws IOException {
         StudentController sc = new StudentController("temp.json");
         Assert.assertNotNull(sc.findByName("nhi"));
     }
 
     @Test
-    public void test_findByClass() {
+    public void test_findByClass() throws IOException {
         StudentController sc = new StudentController("temp.json");
         Assert.assertNotNull(sc.findByClass("15T3"));
     }
 
     @AfterClass
-    public static void finalized() {
-        try {
-            students = WriteReadFile.readJsonFile(file_temp, Student[].class);
-            WriteReadFile.writeToJsonFile(file_origin, students);
-        } catch (IOException e) {
-        }
+    public static void finalized() throws Exception {
+        students = WriteReadFile.readJsonFile(file_temp, Student[].class);
+        WriteReadFile.writeToJsonFile(file_origin, students);
         file_temp.delete();
     }
 }
